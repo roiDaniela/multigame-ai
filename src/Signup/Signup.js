4fb32adc83d897b1ac90b7360863777a7e4554a0
Kobi <abirkoby@gmail.com>
2018-10-04 12:23:41 +0300

    changed api location

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -34,7 +34,7 @@ class Signup extends Component {
             isLoading: true
         })
         console.log(this.props.store.data)
-        fetch('http://localhost:8080/api/account/signup', { 
+        fetch('kobia.xyz:8080/api/account/signup', { 
             method: 'POST', 
             headers: {
                 'Content-Type': 'application/json'
